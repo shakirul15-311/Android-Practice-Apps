fe450951e009d8c881e848e7bd42afba8405371f
shakirul15-311 <shakirul15-311@diu.edu.bd>
2019-04-06 13:18:46 +0600

    https://shakirul15-311.github.io/shakirul.io/ Commit 1

--- NerdCastle/app/src/main/java/com/example/nerdcastle/MainActivity.java
+++ NerdCastle/app/src/main/java/com/example/nerdcastle/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.nerdcastle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.loadUrl("https://nerdcastlebd.com/");
     }
 }
