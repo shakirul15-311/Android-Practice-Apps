dbd7a157dbd82df6236f0b98b8c1f9536b8669b1
shakirul15-311 <shakirul15-311@diu.edu.bd>
2019-04-06 13:30:02 +0600

    shakirul.io/ Commit 1

--- app/src/main/java/com/example/shakirulio/MainActivity.java
+++ app/src/main/java/com/example/shakirulio/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         webView = findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
