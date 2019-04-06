be51b47f3f2bec6ed36be8e41b97c9952f3fd640
shakirul15-311 <shakirul15-311@diu.edu.bd>
2019-04-06 13:26:00 +0600

    shakirul.io/ Commit 1

---
+++ app/src/main/java/com/example/shakirulio/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.shakirulio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://shakirul15-311.github.io/shakirul.io");
+    }
+}
