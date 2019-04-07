8dadbe4b0ce4ffa01d82dad867c99891855f6bc7
shakirul15-311 <shakirul15-311@diu.edu.bd>
2019-04-07 12:22:05 +0600

    ListviewSearch

--- ListView/app/src/main/java/com/example/listview/MainActivity.java
+++ ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -2,26 +2,64 @@ package com.example.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
+    private SearchView searchView;
+    ArrayAdapter<String> adapter;
+    String[] countryNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] countryNames = getResources().getStringArray(R.array.country_lst);
         listView = findViewById(R.id.listViewId);
+        searchView =  findViewById(R.id.searchView);
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.sample_view, R.id.textView, countryNames );
+        countryNames = getResources().getStringArray(R.array.country_list);
+
+
+        adapter = new ArrayAdapter<String>(MainActivity.this, R.layout.sample_view, R.id.textViewID, countryNames );
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                 String value = countryNames[position];
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+
 
 
     }
+
+
 }
---
+++ RecyclerView_SearchView/app/src/main/java/com/example/recyclerview_searchview/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.recyclerview_searchview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private ExampleAdapter adapter;
+    private List<ExampleItem> exampleList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fillExampleList();
+        setUpRecyclerView();
+    }
+
+    private void fillExampleList() {
+        exampleList = new ArrayList<>();
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "One", "Ten"));
+        exampleList.add(new ExampleItem(R.drawable.ic_audio, "Two", "Eleven"));
+        exampleList.add(new ExampleItem(R.drawable.ic_sun, "Three", "Twelve"));
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "Four", "Thirteen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_audio, "Five", "Fourteen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_sun, "Six", "Fifteen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_android, "Seven", "Sixteen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_audio, "Eight", "Seventeen"));
+        exampleList.add(new ExampleItem(R.drawable.ic_sun, "Nine", "Eighteen"));
+    }
+
+    private void setUpRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        adapter = new ExampleAdapter(exampleList);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setImeOptions(EditorInfo.IME_ACTION_DONE);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return true;
+    }
+}
